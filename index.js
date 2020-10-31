b952781ada532d85625260b8adb5a5144dfdf7f4
Chris Gagne <cdgagne@gmail.com>
2020-10-31 16:39:10 -0700

    Exercise 3.5 Phonebook backend step5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let persons = [
     {
@@ -49,6 +52,27 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
     
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'Key Missing: name'
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'Key Missing: number'
+        })
+    }
+    const person = {
+        id: Math.floor(Math.random() * Math.floor(32768)),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    return response.json(person)
 })
 
 const PORT = 3001
