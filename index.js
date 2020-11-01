6d03fa27e02a26f281dadeca22a5da2b83550772
Chris Gagne <cdgagne@gmail.com>
2020-11-01 11:23:13 -0800

    Exercise 3.8 Phonebook backend step8

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+// Use Morgan for logging requests and define a custom token to allow logging of the request body
+morgan.token('req-body', (req, res) =>
+    JSON.stringify(req.body)
+)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 let persons = [
     {
